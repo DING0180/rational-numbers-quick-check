c9f52f8cc61da377b98e51b9beeb5e052901e58c
DING0180 <2507681021@qq.com>
2026-08-22 15:58:31 +0800

    publish: add src/data/lessons.js

---
+++ src/data/lessons.js
@@ -0,0 +1,15 @@
+export const LESSONS = Object.freeze([
+  ['L01', '01', 'Addition Rules', '有理数的加法法则'],
+  ['L02', '02', 'Laws of Addition', '有理数的加法运算律'],
+  ['L03', '03', 'Subtraction Rules', '有理数的减法法则'],
+  ['L04', '04', 'Mixed Addition & Subtraction', '有理数的加减混合运算'],
+  ['L05', '05', 'Multiplication Rules', '有理数的乘法法则'],
+  ['L06', '06', 'Laws of Multiplication', '有理数的乘法运算律'],
+  ['L07', '07', 'Division Rules', '有理数的除法法则'],
+  ['L08', '08', 'Mixed Operations', '有理数的加减乘除混合运算'],
+  ['L09', '09', 'Powers', '乘方'],
+  ['L10', '10', 'Mixed Operations with Powers', '含乘方的有理数混合运算'],
+  ['L11', '11', 'Scientific Notation', '科学记数法'],
+  ['L12', '12', 'Approximation & Precision', '近似数与精确度'],
+  ['L13', '13', 'Number Systems', '进位制的认识与探究']
+].map(([id, number, englishName, chineseName]) => Object.freeze({ id, number, englishName, chineseName })));
